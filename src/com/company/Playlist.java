b2638d96d56f2374bdd88320e555f8623fce1a0f
EstebanGaribello <97629948+EstebanGaribello@users.noreply.github.com>
2022-02-12 10:56:31 -0500

    se creo la clase play list

---
+++ src/com/company/Playlist.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+    private List<Song> songs;
+    private String title;
+
+    public Playlist( String title) {
+        this.songs = new ArrayList<>();
+        this.title = title;
+    }
+
+    public void addSongs(Song song){
+
+        songs.add(song);
+    }
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public String gettitle() {
+        return title;
+    }
+
+    public void setName(String title) {
+        this.title = title;
+    }
+}
